6622ef3f89b45cc69af6a2322757e9b5ec88e380
AlbertXDing <97485489+AlbertXDing@users.noreply.github.com>
2023-05-15 20:15:01 -0700

    Update main.js
    Fixed indentation(dropped my water bottle on my keyboard)

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,21 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const buttons = document.querySelectorAll('button');
+    const buttons = document.querySelectorAll('button');
     const editBox = document.getElementById('editBox');
     const save = document.querySelector('.save');
-	const userInput = document.getElementById('userInput');
+    const userInput = document.getElementById('userInput');
 
-	buttons.forEach((button) => {
+    buttons.forEach((button) => {
         // When a Button is clicked, editBox appears
-		button.addEventListener('click', () => {
-			selectedButton = button;
-			editBox.style.display = 'block';
+	button.addEventListener('click', () => {
+		selectedButton = button;
+		editBox.style.display = 'block';
 
-            // When the save button is clicked, button's text is changed to the input from editBox
-			save.addEventListener('click', () => {
-				button.textContent = userInput.value;
-				editBox.style.display = 'none';
-			});
-
+        // When the save button is clicked, button's text is changed to the input from editBox
+		save.addEventListener('click', () => {
+			button.textContent = userInput.value;
+			editBox.style.display = 'none';
 		});
-	});
+
+	 });
+     });
 });
