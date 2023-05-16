114a9dd62210617592b2dade5d3e8a4d7619176e
Albert Ding <axding@ucsd.edu>
2023-05-15 20:05:22 -0700

    Added prompts and gave users ability to change prompts
    Sidebar with eight previously generated prompts. If the user clicks on one of them, a prompt appears where the user can change the prompts.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,21 @@
-// main.js
-
 document.addEventListener('DOMContentLoaded', () => {
-    // init actions
+	const buttons = document.querySelectorAll('button');
+    const editBox = document.getElementById('editBox');
+    const save = document.querySelector('.save');
+	const userInput = document.getElementById('userInput');
+
+	buttons.forEach((button) => {
+        // When a Button is clicked, editBox appears
+		button.addEventListener('click', () => {
+			selectedButton = button;
+			editBox.style.display = 'block';
+
+            // When the save button is clicked, button's text is changed to the input from editBox
+			save.addEventListener('click', () => {
+				button.textContent = userInput.value;
+				editBox.style.display = 'none';
+			});
+
+		});
+	});
 });
