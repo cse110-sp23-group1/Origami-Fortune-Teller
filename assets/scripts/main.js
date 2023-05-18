38e1b6d4afda5fb83835be3b3b7242157666aace
phentos <40075433+phentos@users.noreply.github.com>
2023-05-17 23:46:24 -0700

    fixes #24 fixes #23, misc refactors

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,29 +1,150 @@
-//Upon Page load, call activateSidebarHandler()
+// main.js
+/* 
+    Calls on page load
+*/
 document.addEventListener('DOMContentLoaded', () => {
     activateSidebarHandler();
 });
 
-// If any button is clicked in the sidebar, call editFortuneBox(button), which takes an argument of the button that was clicked
-function activateSidebarHandler() {
-    const sidebarButtons = document.querySelectorAll('.sidebarButton');
+
+// TODO: move to fortunes.json, create fortune
+// loading handler and update button creation.
+// placeholder values:
+const defaultFortunes = [
+    "Outlook not so good",
+    "Signs point to yes",
+    "Cannot predict now",
+    "Reply hazy, try again later",
+    "It is certain",
+    "Don't Count on it",
+    "Better not tell you now",
+    "As I see it, yes"
+]
+
+/*
+    Handler for the fortune sidebar
+
+    The sidebar needs to:
+        - position buttons/accept edit requests
+        - recive changes and update button values
     
-    sidebarButtons.forEach((button) => {
+    to do so, it uses a helper function for each
+*/
+function activateSidebarHandler() {    
+    activateSidebarButtons();
+    activateFortuneInputHandler();
+}
+
+/*
+    The sidebar is a vertical stack of buttons.
+
+    We create the buttons here (to reduce styles and html
+    clutter) then enable clicking to input new 
+    fortunes.
+*/
+function activateSidebarButtons(){
+    const buttonHeight = 73;
+
+    // using defaultFortunes is a convenient 
+    // placeholder pending externalizing fortune 
+    // values and modularizing defaults
+    defaultFortunes.forEach((fortune, index) => {
+        const button = document.createElement('button');
+        button.textContent = fortune;
+
+        button.style.top = `${index*buttonHeight}px`;
         button.addEventListener('click', () => {
-            editFortune(button);
+            openFortuneInput(index);
         });
+        
+        getSidebar().appendChild(button);
     });
 }
 
-// Make the editFortuneBox appear, allow the user to write in their own fortune, and change the text of the button that was passed in
-function editFortune(button){
-    const editFortuneBox = document.getElementById('editFortuneBox');
-    const saveButton = document.querySelector('.saveButton');
-    const userInput = document.getElementById('userInput');
+/*
+    Set up the fortuneInputBox and display it.
+*/
+function openFortuneInput(buttonIndex){    
+    getFortuneTextInput().value = getSidebarButtonContent(buttonIndex);
+    getFortuneInputSaveButton().id = buttonIndex;
+    getFortuneInputBox().style.display = 'block';
+}
 
-    editFortuneBox.style.display = 'block';
+/*
+    Tears down the fortuneInputBox and optionally
+    by parameter passes along its value to the
+    button which activated it.
 
-    saveButton.addEventListener('click', () => {
-        button.textContent = userInput.value;
-        editFortuneBox.style.display = 'none';
+    @param (bool) needToSubmitFortune
+*/
+function closeFortuneInput(needTosubmitFortune=false){
+    getFortuneInputBox().style.display = 'none';
+
+    if (!needTosubmitFortune) { return; }
+    else {
+        const saveButton = getFortuneInputSaveButton();
+        const userInput = getFortuneTextInput();
+    
+        setSidebarButtonContent(saveButton.id, userInput.value);
+        userInput.value = "";
+    
+        saveButton.id = null;
+    }
+}
+
+/*
+    Handler for the fortuneInputBox
+
+    submits when:
+        - enter key in text input
+        - changing fortune and losing focus
+        - click save button
+
+    doesn't submit when:
+        - escape key
+*/
+function activateFortuneInputHandler(){
+    getFortuneTextInput().addEventListener('keyup', (event) => {
+        if (event.key === 'Enter') closeFortuneInput(true);
     });
+
+    window.addEventListener('keyup', (event) => {
+        if (event.key === 'Escape') closeFortuneInput();
+    })
+
+    getFortuneTextInput().addEventListener('change', (event) => {
+        closeFortuneInput(true);
+    });
+
+    getFortuneInputSaveButton().addEventListener('click', () => {
+        closeFortuneInput(true);
+    });
+}
+
+function getFortuneInputSaveButton(){
+    return document.querySelector('.fortuneInputBox .saveButton');
+}
+
+function getFortuneInputBox(){
+    return document.querySelector('.fortuneInputBox');
+}
+
+function getFortuneTextInput(){
+    return document.querySelector('.fortuneInputBox input');
+}
+
+function getSidebar(){
+    return document.querySelector('.sidebar');
+}
+
+function getSidebarButtons(){
+    return document.querySelectorAll('.sidebar button');
+}
+
+function getSidebarButtonContent(index){
+    return getSidebarButtons()[index].textContent;
+}
+
+function setSidebarButtonContent(index, content){
+    getSidebarButtons()[index].textContent = content;
 }
