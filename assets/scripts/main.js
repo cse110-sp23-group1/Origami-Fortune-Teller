bdffee57fc00492c386b6ee0c423667e49fb406f
phentos <40075433+phentos@users.noreply.github.com>
2023-05-22 12:16:06 -0700

    populate fortunes.json, lint main

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,25 +1,24 @@
 // main.js
-/* 
+/*
     Calls on page load
 */
 document.addEventListener('DOMContentLoaded', () => {
-    activateSidebarHandler();
+  activateSidebarHandler();
 });
 
-
-// TODO: move to fortunes.json, create fortune
-// loading handler and update button creation.
+// TODO: create fortune loading handler
+// and update button creation.
 // placeholder values:
 const defaultFortunes = [
-    "Outlook not so good",
-    "Signs point to yes",
-    "Cannot predict now",
-    "Reply hazy, try again later",
-    "It is certain",
-    "Don't Count on it",
-    "Better not tell you now",
-    "As I see it, yes"
-]
+  'Outlook not so good',
+  'Signs point to yes',
+  'Cannot predict now',
+  'Reply hazy, try again later',
+  'It is certain',
+  'Don\'t Count on it',
+  'Better not tell you now',
+  'As I see it, yes',
+];
 
 /*
     Handler for the fortune sidebar
@@ -27,47 +26,47 @@ const defaultFortunes = [
     The sidebar needs to:
         - position buttons/accept edit requests
         - recive changes and update button values
-    
+
     to do so, it uses a helper function for each
 */
-function activateSidebarHandler() {    
-    activateSidebarButtons();
-    activateFortuneInputHandler();
+function activateSidebarHandler() {
+  activateSidebarButtons();
+  activateFortuneInputHandler();
 }
 
 /*
     The sidebar is a vertical stack of buttons.
 
     We create the buttons here (to reduce styles and html
-    clutter) then enable clicking to input new 
+    clutter) then enable clicking to input new
     fortunes.
 */
-function activateSidebarButtons(){
-    const buttonHeight = 73;
+function activateSidebarButtons() {
+  const buttonHeight = 73;
 
-    // using defaultFortunes is a convenient 
-    // placeholder pending externalizing fortune 
-    // values and modularizing defaults
-    defaultFortunes.forEach((fortune, index) => {
-        const button = document.createElement('button');
-        button.textContent = fortune;
+  // using defaultFortunes is a convenient
+  // placeholder pending externalizing fortune
+  // values and modularizing defaults
+  defaultFortunes.forEach((fortune, index) => {
+    const button = document.createElement('button');
+    button.textContent = fortune;
 
-        button.style.top = `${index*buttonHeight}px`;
-        button.addEventListener('click', () => {
-            openFortuneInput(index);
-        });
-        
-        getSidebar().appendChild(button);
+    button.style.top = `${index*buttonHeight}px`;
+    button.addEventListener('click', () => {
+      openFortuneInput(index);
     });
+
+    getSidebar().appendChild(button);
+  });
 }
 
 /*
     Set up the fortuneInputBox and display it.
 */
-function openFortuneInput(buttonIndex){    
-    getFortuneTextInput().value = getSidebarButtonContent(buttonIndex);
-    getFortuneInputSaveButton().id = buttonIndex;
-    getFortuneInputBox().style.display = 'block';
+function openFortuneInput(buttonIndex) {
+  getFortuneTextInput().value = getSidebarButtonContent(buttonIndex);
+  getFortuneInputSaveButton().id = buttonIndex;
+  getFortuneInputBox().style.display = 'block';
 }
 
 /*
@@ -77,19 +76,20 @@ function openFortuneInput(buttonIndex){
 
     @param (bool) needToSubmitFortune
 */
-function closeFortuneInput(needTosubmitFortune=false){
-    getFortuneInputBox().style.display = 'none';
+function closeFortuneInput(needTosubmitFortune=false) {
+  getFortuneInputBox().style.display = 'none';
 
-    if (!needTosubmitFortune) { return; }
-    else {
-        const saveButton = getFortuneInputSaveButton();
-        const userInput = getFortuneTextInput();
-    
-        setSidebarButtonContent(saveButton.id, userInput.value);
-        userInput.value = "";
-    
-        saveButton.id = null;
-    }
+  if (!needTosubmitFortune) {
+    return;
+  } else {
+    const saveButton = getFortuneInputSaveButton();
+    const userInput = getFortuneTextInput();
+
+    setSidebarButtonContent(saveButton.id, userInput.value);
+    userInput.value = '';
+
+    saveButton.id = null;
+  }
 }
 
 /*
@@ -103,48 +103,48 @@ function closeFortuneInput(needTosubmitFortune=false){
     doesn't submit when:
         - escape key
 */
-function activateFortuneInputHandler(){
-    getFortuneTextInput().addEventListener('keyup', (event) => {
-        if (event.key === 'Enter') closeFortuneInput(true);
-    });
+function activateFortuneInputHandler() {
+  getFortuneTextInput().addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') closeFortuneInput(true);
+  });
 
-    window.addEventListener('keyup', (event) => {
-        if (event.key === 'Escape') closeFortuneInput();
-    })
+  window.addEventListener('keyup', (event) => {
+    if (event.key === 'Escape') closeFortuneInput();
+  });
 
-    getFortuneTextInput().addEventListener('change', (event) => {
-        closeFortuneInput(true);
-    });
+  getFortuneTextInput().addEventListener('change', (event) => {
+    closeFortuneInput(true);
+  });
 
-    getFortuneInputSaveButton().addEventListener('click', () => {
-        closeFortuneInput(true);
-    });
+  getFortuneInputSaveButton().addEventListener('click', () => {
+    closeFortuneInput(true);
+  });
 }
 
-function getFortuneInputSaveButton(){
-    return document.querySelector('.fortuneInputBox .saveButton');
+function getFortuneInputSaveButton() {
+  return document.querySelector('.fortuneInputBox .saveButton');
 }
 
-function getFortuneInputBox(){
-    return document.querySelector('.fortuneInputBox');
+function getFortuneInputBox() {
+  return document.querySelector('.fortuneInputBox');
 }
 
-function getFortuneTextInput(){
-    return document.querySelector('.fortuneInputBox input');
+function getFortuneTextInput() {
+  return document.querySelector('.fortuneInputBox input');
 }
 
-function getSidebar(){
-    return document.querySelector('.sidebar');
+function getSidebar() {
+  return document.querySelector('.sidebar');
 }
 
-function getSidebarButtons(){
-    return document.querySelectorAll('.sidebar button');
+function getSidebarButtons() {
+  return document.querySelectorAll('.sidebar button');
 }
 
-function getSidebarButtonContent(index){
-    return getSidebarButtons()[index].textContent;
+function getSidebarButtonContent(index) {
+  return getSidebarButtons()[index].textContent;
 }
 
-function setSidebarButtonContent(index, content){
-    getSidebarButtons()[index].textContent = content;
+function setSidebarButtonContent(index, content) {
+  getSidebarButtons()[index].textContent = content;
 }
