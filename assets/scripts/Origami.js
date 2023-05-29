e98c9f4dabb559240746c99ec9531ff83247eaee
adarsh249 <adarshpatel249@gmail.com>
2023-05-28 18:26:44 -0700

    Fixed up Origami class, cleaned up main.js, sidenote: gonna eat dinner then get back on to fix this up

--- assets/scripts/Origami.js
+++ assets/scripts/Origami.js
@@ -8,77 +8,73 @@
 export class Origami {
   /**
    * @constructor
-   * @param {*} svgPaths - array of svg paths
+   * @param {>} svgPath - an svg path
    */
-  constructor(svgPaths) {
+  constructor(svgPath) {
     //this number will change when we add SVG
-    if(svgPaths.length !== 5){
-      throw new Error("Not All Paths Fetched Here.");
-    }
-    this.fortuneSVGs = [];
-    this.clickableSVGs = [];
-    this.#init(); 
-
+    this.svgPath = svgPath;
+    this.flapColorClicked = null;
+    this.#init();
   }
-
   #init() {
-    this.clickableSVG.push("./assets/images/origami/closed.svg");
-    this.clickableSVG.push("./assets/images/origami/horizontally-opened-nums.svg");
-    this.clickableSVG.push("./assets/images/origami/vertically-opened-nums.svg");
-    this.#generateSVG();
-    //this.clickableSVG.push("lastfigma of all 8 fortunes to select");
+    this.generateSVG();
+    this.#addClickListeners();
   }
-  #generateSVG() {
-    const closed = document.createElement("object");
-    closed.data = this.clickableSVG[0];
-    this.container.appendChild(closed);
+  generateSVG() {
+    const svg = document.createElement("object");
+    svg.data = this.svgPath;
+    document.body.appendChild(svg);
+    this.currentSVG = svg;
   }
-  /*
-      TODO: for an example, see SideBar.js
 
-      in short, if the constructor stored the different SVGs, you
-        can iterate on them to set events for each click layer. there
-        are a lot of different ways to implement this so go with
-        whatever makes the most sense
-  */
-  setClosedClickHandler() {
-    const closedFortune = this.clickableSVG[0];
-    const closedFortuneFlaps = closedFortune.querySelectorAll('g[id$="-default"]');
-    closedFortuneFlaps.forEach((flap) => {
-      let flapClick = flap.querySelector('[id$="-click"]');
-      flapClick.addEventListener("click", () => {
-        console.log("This is working");
-        //add what we want to happen when u click
+  removeCurrentSVG() {
+    this.currentSVG.remove();
+    this.currentSVG = null;
+  }
+
+  #addClickListeners() {
+    if(this.currentSVG.data.endsWith('closed-new.svg')){
+      this.activateClosedHandler();
+    }
+    else if(this.currentSVG.data.endsWith('-nums.svg')){
+      //activateNumsHandler();
+    }
+  }
+  idToColor(flapID) {
+    switch(flapID) {
+      case 'upper-right-click':
+        return "blue";
+      case 'upper-left-click':
+        return "red";
+      case 'lower-left-click':
+        return "green";
+      case 'lower-right-click':
+        return "yellow";
+      default:
+        return "unknown";
+    }
+  }
+  activateClosedHandler() {
+    return new Promise((resolve) => {
+      this.currentSVG.addEventListener('load', () => {
+        let svgDocument = this.currentSVG.contentDocument;
+        const closedFlaps = svgDocument.querySelectorAll('g[id$="-flap-d"] path[id$="-click"]');
+        closedFlaps.forEach((flap) => {
+          flap.addEventListener('click', (event) => {
+            let flapID = flap.id;
+            this.flapColorClicked = this.idToColor(flapID);
+            resolve(this.flapColorClicked);
+          });
+        });
       });
     });
-    
   }
 
-  // }
+  async getFlapColorClicked() {
+    return await this.activateClosedHandler();
+  }
 
-  /*
-    TODO: when Origami.getSVG is called, it could return
-      the current mode's origami as an SVG HTMLElement
-    
-    an alternate approach would be to take a container as an constructor
-    parameter and modify that instead
-  */
-  // getSVG(){
-
-  // }
-
-  /*
-    TODO: when Origami.setMode is called, it could
-      update the current display mode and then
-      replace the currently displayed origami in
-      this.container with the one matching the input
-      parameter
-    TODO: decide on mode parameter type, maybe string,
-      or even consider having a function for each mode select
-      e.g. Origami.close() Origami.displayNumbers() etc
-  */
-  // setMode(newMode){
-  //   //  make sure the newMode is valid
-  //   //  if it is, set the current mode to it
-  // }
+  startAnimation(flapColor) {
+    return flapColor.length;
+  }
 }
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,100 +1,14 @@
-/*
-// Upon Page load, call activateSidebarHandler()
-document.addEventListener('DOMContentLoaded', () => {
-  activateSidebarHandler();
+import { Origami } from './Origami.js';
+const svgPaths = [
+  "./assets/images/origami-with-ids/closed-new.svg",
+  "./assets/images/origami-with-ids/horizontally-opened-nums.svg",
+  "./assets/images/origami-with-ids/vertically-opened-nums.svg",
+  "./assets/images/origami-with-ids/horizontally-opened.svg",
+  "./assets/images/origami-with-ids/vertically-opened.svg"
+]
+const closedSVG = new Origami(svgPaths[0]);
+
+closedSVG.getFlapColorClicked().then((flapColorClicked) => {
+  let numAnimations = closedSVG.startAnimation(flapColorClicked);
+  console.log(numAnimations);
 });
-
-// If any button is clicked in the sidebar, call editFortuneBox(button), which takes an argument of the button that was clicked
-function activateSidebarHandler() {
-  const sidebarButtons = document.querySelectorAll('.sidebarButton');
-
-  sidebarButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      editFortune(button);
-    });
-  });
-}
-
-// Make the editFortuneBox appear, allow the user to write in their own fortune, and change the text of the button that was passed in
-function editFortune(button) {
-  const editFortuneBox = document.getElementById('editFortuneBox');
-  const saveButton = document.querySelector('.saveButton');
-  const userInput = document.getElementById('userInput');
-
-  editFortuneBox.style.display = 'block';
-
-  saveButton.addEventListener('click', () => {
-    button.textContent = userInput.value;
-    editFortuneBox.style.display = 'none';
-  });
-}
-*/
-let svgPaths;
-let svgDocument;
-
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('assets/SVGPaths/svgPaths.json')
-  .then((response) => response.json())
-  .then((data) => {
-    svgPaths = data.filePaths;
-    activateSVGHandler();
-  })
-  .catch((error) => {
-    console.error('Error fetching fortunes:', error);
-  });
-});
-
-
-function activateSVGHandler() {
-  activateClosedHandler();
-  //activateNumberHandler();
-  //activateFortuneClickHandler();
-}
-
-function activateClosedHandler() {
-  const closedFortune = document.createElement('object');
-  closedFortune.data = svgPaths.closed;
-  closedFortune.id = "default";
-  getFortuneTeller().appendChild(closedFortune);
-  // must load SVG content fully before accessing it
-  closedFortune.addEventListener('load', function() {
-    svgDocument = closedFortune.contentDocument;
-    const closedFlaps = svgDocument.querySelectorAll('g[id$="-flap-d"] path[id$="-click"]');
-    closedFlaps.forEach((flap) => {
-      flap.addEventListener('click', (event) => {
-        let flapID = event.target.getAttribute('id');
-        startAnimation(flapID);
-      });
-    });
-  });
-  // activateClosedFlapClickHandler();
-}
-// function activateClosedFlapClickHandler() {
-//   // console.log(document.getElementById('default').contentDocument);
-
-// }
-function startAnimation(flapID) {
-  switch(flapID) {
-    case 'upper-left-click':
-      console.log("Red Flap Clicked");
-      break;
-    case 'lower-left-click':
-      console.log("Green Flap Clicked");
-      break;
-    case 'upper-right-click':
-      console.log("Blue Flap Clicked");
-      break;
-    case 'lower-right-click':
-      console.log("Yellow Flap Clicked");
-      break;
-    default:
-      console.log("Click Random");
-      break;
-  }
-}
-function getFortuneTeller() {
-  return document.getElementById("closedFortune");
-}
-
-// function getClosedFlaps() {
-// }
