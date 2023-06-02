33428a5b26324126cf5efea47c16773d220d808f
lmeiucsd <88351375+lmeiucsd@users.noreply.github.com>
2023-06-01 21:07:20 -0700

    origami image/operations (#19)
    * exported fortune teller states as SVG
    
    * cleaned up SVGs, renamed folder to 'origami'
    
    * pretty print the SVGs
    
    * Updating Format of SVG so its easier to read
    
    * Fixed svgs with IDs
    
    * add shell for svg handler as Origami.js
    
    * added html and css to display SVG on web page
    
    * zipped files
    
    * Pushing new svg exports
    
    * Working on making clickable flaps... queryselector issues
    
    * fixed queryselector issue, flaps respond to clicks
    
    * Created variable to understand which flap we have clicked.
    
    * Fixed up Origami class, cleaned up main.js, sidenote: gonna eat dinner then get back on to fix this up
    
    * Starting simple stop motin animation
    
    * Got basic animations working
    
    * Basic functionality of SVG complete
    
    * switch cases begone
    
    * Updated Origami svg
    
    Changed size for horizontally opened(no nums) and vertically opened(no nums). As well as added origami state with fortune
    
    * Updated filepaths for new Figma SVGs
    
    * Centered fortune teller
    
    * added documentation for Origami.js and main.js
    
    * sync gitignore with main
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * accidentally deleted wrong svg files lol, fixed now
    
    * Fixed style and filenames of SVGs
    
    * Cleaned up code a bit more, added all code to Origami.js, animation is still incredibly buggy, working on correcting this behavior
    
    * Created basic functionality of animations, small bugs with vertical/horizontal, need to still clean up code.
    
    * Removing text off Figma mockups
    
    * Fixed bug where user could click multiple times, added a darkening function and added this to a hover function on all clickable elements, saved Fortunes on first click to LocalStorage, removed sidebar when user starts animation, chose a random fortune from localStorage when user clicks final flap, adding files from sidebar branch to test svg core functionality.
    
    * Removed unncessary array
    
    * Added helper functions so if statements dont look too boggled
    
    * Fixed opening flap becoming smaller
    
    * added docs for updated Origami.js
    
    * forgot to add file from git pull, this fixes size issues of SVGs
    
    * Working on getting text on the final flaps
    
    * ReExported #-opened.svg to get text as <text> instead of <path>, also got text to appear on flap to work for only one flap
    
    * Text works on every flap, fixed svg bug
    
    * Fix InputFortineBox display bug when animations starts
    
    * fixed svg bug that displays the wrong color when you hover your cursor over the inner flap while it's animating and then move your mouse out of it
    
    * fixed hover bug for fully opened SVG
    
    * delete console.log debug statements
    
    * added and updated comments on my bug fix implementation
    
    * Cursor Pointer styling for svg hover effect
    
    * reexported more svgs with text instead of path, created hover bug again
    
    * fixed hover bug on fully opened SVG
    
    * lint fixing
    
    ---------
    
    Co-authored-by: phentos <40075433+phentos@users.noreply.github.com>
    Co-authored-by: adarsh249 <adarshpatel249@gmail.com>
    Co-authored-by: Sam Wong <97695769+samw0627@users.noreply.github.com>
    Co-authored-by: Albert Ding <axding@ucsd.edu>
    Co-authored-by: Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>

---
+++ assets/scripts/Origami.js
@@ -0,0 +1,303 @@
+// Origami.js
+import {saveFortunesOnClick} from './SideBar.js';
+const COLOR_BY_CLICK_REGION = {
+  '#0D6E8E': 'blue',
+  '#BF3858': 'red',
+  '#04AB80': 'green',
+  '#CCA751': 'yellow',
+};
+const NUMBER_TO_DARK_COLOR = {
+  '1': '#073E51',
+  '2': '#073E51',
+  '3': '#917535',
+  '4': '#917535',
+  '5': '#00664C',
+  '6': '#00664C',
+  '7': '#962B44',
+  '8': '#962B44',
+};
+const DARK_COLOR_TO_COLOR = {
+  '#118AB2': '#0D6E8E', // blue
+  '#962B44': '#BF3858', // red
+  '#00664C': '#04AB80', // green
+  '#FFD166': '#CCA751', // yellow
+};
+const svgPaths = [
+  'assets/images/origami/closed.svg',
+  'assets/images/origami/horizontally-opened-nums.svg',
+  'assets/images/origami/vertically-opened-nums.svg',
+  'assets/images/origami/horizontally-opened.svg',
+  'assets/images/origami/vertically-opened.svg',
+  'assets/images/origami/opened.svg',
+  'assets/images/origami/1-opened.svg',
+  'assets/images/origami/2-opened.svg',
+  'assets/images/origami/3-opened.svg',
+  'assets/images/origami/4-opened.svg',
+  'assets/images/origami/5-opened.svg',
+  'assets/images/origami/6-opened.svg',
+  'assets/images/origami/7-opened.svg',
+  'assets/images/origami/8-opened.svg',
+];
+
+/*
+  A class to support origami SVG selection,
+  display, operation
+*/
+export class Origami {
+  /**
+   * @constructor
+   * @param {>} svgPath - an svg path
+   */
+  constructor() {
+    // this number will change when we add SVG
+    this.CLOSEDSVG = svgPaths[0];
+    this.OPENEDHORIZONTALNUMS = svgPaths[1];
+    this.OPENEDVERTICALNUMS = svgPaths[2];
+    this.OPENEDHORIZONTAL = svgPaths[3];
+    this.OPENEDVERTICAL = svgPaths[4];
+    this.OPENEDALL = svgPaths[5];
+    this.currentSVGPath = null;
+    this.CURRENTSVG = null;
+    this.currentTurn = 0;
+    this.currFlapColor = null;
+    this.fortunes = null;
+    this.isAnimationRunning = false;
+    this.#init();
+  }
+  #init() {
+    this.generateSVG(this.CLOSEDSVG);
+  }
+  /**
+   * Creates object for the current SVG and adds click listeners.
+   * @param {string} currentSVGPath - string that represents path of SVG file in our repo
+   */
+  generateSVG(currentSVGPath) {
+    this.currentSVGPath = currentSVGPath;
+    this.CURRENTSVG = document.querySelector('object');
+    if (this.CURRENTSVG.hasAttribute('data')) {
+      this.CURRENTSVG.removeAttribute('data');
+    }
+    this.CURRENTSVG.data = this.currentSVGPath;
+    this.#addClickListeners();
+  }
+  /**
+   * Activates handler for closed SVG or SVG with nums.
+   */
+  #addClickListeners() {
+    if (this.isClosed() || this.isHorizontalWithNums() || this.isVerticalWithNums() || this.isOpen()) {
+      this.activateHandler();
+    }
+  }
+  /**
+   * Adds click and hover functionality event listeners for the current SVG.
+   */
+  activateHandler() {
+    this.CURRENTSVG.addEventListener('load', () => {
+      const flaps = this.CURRENTSVG.contentDocument.querySelectorAll('path[id$="-click"]');
+      flaps.forEach((flap) => {
+        flap.addEventListener('click', this.handleFlapClick);
+        flap.addEventListener('mouseover', this.handleFlapMouseOver);
+        flap.addEventListener('mouseout', this.handleFlapMouseOut);
+      });
+    });
+  }
+
+  /**
+  * Handles the mouse over event for a flap element.
+  * Updates the fill attribute of the event target to a darker shade of a current flap color.
+  * @param {Event} event - The mouse over event object.
+  */
+  handleFlapMouseOver = (event) => {
+    const currColor = event.target.getAttribute('fill');
+    this.currFlapColor = currColor;
+    const darkerShade = this.getDarkerShade(currColor);
+    event.target.setAttribute('fill', darkerShade);
+    event.target.style.cursor = 'pointer';
+  };
+
+  /**
+  * Handles the mouse out event for a flap element.
+  * Updates the fill attribute of the event target to the current flap color.
+  * Handles logic for consistent colors when cursor hovering over inner flaps.
+  * @param {Event} event - The mouse out event object.
+  */
+  handleFlapMouseOut = (event) => {
+    // ensures that original color is kept if cursor hovers over an inner flap in opened SVGs.
+    if (this.currentTurn == 1) { // SVGs with numbers
+      const originalColor = NUMBER_TO_DARK_COLOR[event.target.getAttribute('id').substring(0, 1)]; // array element represents number of flap.
+      event.target.setAttribute('fill', originalColor);
+      return;
+    }
+    if (this.currentTurn == 2) { // fully opened SVG
+      const lightColor = DARK_COLOR_TO_COLOR[event.target.getAttribute('fill')];
+      // if getAttribute did not return any of the dark colors, set color to previous color.
+      if (lightColor == null) {
+        const originalColor = DARK_COLOR_TO_COLOR[NUMBER_TO_DARK_COLOR[event.target.getAttribute('id').substring(0, 1)]];
+        // if user hovers out of flap with a dark color, correct the color
+        if (this.getDarkerShade(event.target.getAttribute('fill')) == originalColor) {
+          this.currFlapColor = originalColor;
+          event.target.setAttribute('fill', this.getOriginalColor(this.currFlapColor));
+          return;
+        }
+        event.target.setAttribute('fill', this.currFlapColor);
+        return;
+      }
+      // if getAttribute returned a dark color, then lightColor is equal to getDarkerShade(originalColor).
+      event.target.setAttribute('fill', this.getOriginalColor(lightColor));
+      return;
+    }
+
+    // otherwise, just set it to previous color.
+    event.target.setAttribute('fill', this.currFlapColor);
+  };
+
+  /**
+  * Handles the click event for a flap element.
+  * Calculates number of animations based on which flap is clicked.
+  * Performs different actions based on the state of the flap element.
+  * If the current SVG is closed, then remove sidebar after a flap is clicked and start animation
+  * If current SVG is opened with numbers, then start animation.
+  * If the current SVG is completely opened, then open flap and display fortune.
+  * @param {Event} event - The click event object.
+  */
+  handleFlapClick = (event) => {
+    event.target.removeEventListener('click', this.handleFlapClick);
+    if (this.isClosed()) {
+      this.fortunes = saveFortunesOnClick();
+      const numAnimations = COLOR_BY_CLICK_REGION[event.target.getAttribute('fill')].length;
+      const sideBar = document.querySelector('.sidebar');
+      if (sideBar) {
+        sideBar.style.display = 'none';
+      }
+      this.currentTurn++;
+      this.startAnimation(numAnimations);
+    } else if (this.isVerticalWithNums() || this.isHorizontalWithNums()) {
+      const numAnimations = parseInt(event.target.id[0]);
+      this.currentTurn++;
+      this.startAnimation(numAnimations);
+    } else if (this.isOpen()) {
+      const flapToOpen = parseInt(event.target.id[0]);
+      this.openFlap(flapToOpen);
+    }
+  };
+  /**
+   * Calculates and returns a darker shade of the given color.
+   * @param {string} color - The color in hexadecimal format (#RRGGBB).
+   * @return {string} The darker shade of the color in hexadecimal format (#RRGGBB).
+   */
+  getDarkerShade(color) {
+    const darkFactor = 0.8;
+    const r = parseInt(color.substr(1, 2), 16);
+    const g = parseInt(color.substr(3, 2), 16);
+    const b = parseInt(color.substr(5, 2), 16);
+    const darkR = Math.floor(r * darkFactor);
+    const darkG = Math.floor(g * darkFactor);
+    const darkB = Math.floor(b * darkFactor);
+    let newColor = `#${darkR.toString(16).padStart(2, '0')}${darkG.toString(16).padStart(2, '0')}${darkB.toString(16).padStart(2, '0')}`;
+    newColor = newColor.toUpperCase();
+    return newColor;
+  }
+
+  /**
+   * Calculates and returns the original color when a darker shade is passed.
+   * @param {string} darkerColor - The darker shade color in hexadecimal format (#RRGGBB).
+   * @return {string} The original color in hexadecimal format (#RRGGBB).
+   */
+  getOriginalColor(darkerColor) {
+    const darkFactor = 0.8;
+    const darkR = parseInt(darkerColor.substr(1, 2), 16);
+    const darkG = parseInt(darkerColor.substr(3, 2), 16);
+    const darkB = parseInt(darkerColor.substr(5, 2), 16);
+    const r = Math.ceil(darkR / darkFactor);
+    const g = Math.ceil(darkG / darkFactor);
+    const b = Math.ceil(darkB / darkFactor);
+    let originalColor = `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
+    originalColor = originalColor.toUpperCase();
+    return originalColor;
+  }
+
+  /**
+   * Opens the specified flap and displays fortune.
+   * @param {number} flapToOpen - The number of the flap to open.
+   */
+  openFlap(flapToOpen) {
+    this.generateSVG('./assets/images/origami/' + flapToOpen + '-Opened.svg');
+    const randomFortune = this.fortunes[Math.floor(Math.random() * 7)];
+    this.CURRENTSVG.onload = () => {
+      const fortuneTextElement = this.CURRENTSVG.contentDocument.querySelector('#fortuneText');
+      fortuneTextElement.textContent = randomFortune;
+    };
+  }
+
+  /**
+   * Starts the animation with the specified number of animations and handles animation logic.
+   * Origami animates between horizontally and vertically opened SVGs every 500ms, for a total of numAnimations - 1 times.
+   * Then it generates the next current SVG and displays it on the webpage.
+   * @param {number} numAnimations - The number of animations to perform.
+   */
+  startAnimation(numAnimations) {
+    let count = 1;
+    const interval = setInterval(() => {
+      this.isAnimationRunning = true;
+      if (count >= numAnimations) {
+        clearInterval(interval);
+        // if the last animation is horizontal, show vertical w nums
+        if (this.isHorizontal() && this.currentTurn < 2) {
+          // if last animation is vertical, show horizontal w nums
+          this.generateSVG(this.OPENEDVERTICALNUMS);
+        } else if (this.isVertical() && this.currentTurn < 2) {
+          this.generateSVG(this.OPENEDHORIZONTALNUMS);
+        } else {
+          this.generateSVG(this.OPENEDALL);
+        }
+        count = 1;
+        this.isAnimationRunning = false;
+        return;
+      }
+      // if file is closed, show vertical opened
+      if (this.isClosed()) {
+        this.generateSVG(this.OPENEDVERTICAL);
+      } else if (this.isHorizontal()) {
+        // if file is horizontal, show vertical opened
+        if (this.currentTurn === 2) {
+          this.generateSVG(this.OPENEDALL);
+        }
+        this.generateSVG(this.OPENEDVERTICAL);
+      } else if (this.isVertical()) {
+        // if file is vertical, show horizontal opened
+        this.generateSVG(this.OPENEDHORIZONTAL);
+      } else if (this.isHorizontalWithNums()) {
+        // if file is horizontal w nums, show horizontal opened
+        this.generateSVG(this.OPENEDVERTICAL);
+      } else if (this.isVerticalWithNums()) {
+        // if file is vertical w nums, show vertical opened
+        this.generateSVG(this.OPENEDHORIZONTAL);
+      }
+      count++;
+    }, 500);
+  }
+
+  isVertical() {
+    return this.CURRENTSVG.data.endsWith(svgPaths[4]);
+  }
+
+  isVerticalWithNums() {
+    return this.CURRENTSVG.data.endsWith(svgPaths[2]);
+  }
+
+  isHorizontal() {
+    return this.CURRENTSVG.data.endsWith(svgPaths[3]);
+  }
+
+  isHorizontalWithNums() {
+    return this.CURRENTSVG.data.endsWith(svgPaths[1]);
+  }
+
+  isClosed() {
+    return this.CURRENTSVG.data.endsWith(svgPaths[0]);
+  }
+
+  isOpen() {
+    return this.CURRENTSVG.data.endsWith(svgPaths[5]);
+  }
+}
