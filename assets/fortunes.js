426a332d7dc48fb7a555563f55f5b6fd787dd361
zsiyed <69491969+zsiyed@users.noreply.github.com>
2023-06-02 18:32:30 -0700

    Complete core fortune sidebar functionality (#52)
    * fetchFortunes from json
    
    * add comments on fetchFortunes
    
    * fix url
    
    * fix import typo
    
    ---------
    
    Co-authored-by: parth4apple <worldrecord00@gmail.com>

---
+++ assets/fortunes.js
@@ -0,0 +1,14 @@
+export const fortunes = {
+  'english': {
+    'default': [
+      'Outlook not so good',
+      'Signs point to yes',
+      'Cannot predict now',
+      'Reply hazy, try again later',
+      'It is certain',
+      'Don\'t Count on it',
+      'Better not tell you now',
+      'As I see it, yes',
+    ],
+  },
+};
