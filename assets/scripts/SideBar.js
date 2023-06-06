a63770cfe679eecd56e07d824e9f838fd32108e9
parth4apple <72187062+parth4apple@users.noreply.github.com>
2023-06-06 00:33:44 -0700

    Moved sidebar to bottom + more responsiveness

--- assets/scripts/SideBar.js
+++ assets/scripts/SideBar.js
@@ -32,27 +32,12 @@ export class SideBar {
    * @private
    */
   #generateButtons() {
-    this.textValues.forEach((text, index) => {
+    this.textValues.forEach((text) => {
       const button = document.createElement('button');
       button.textContent = text;
-      button.style.top = `${index*this.buttonHeight}px`;
     });
 
     this.buttons.push(button);
-  }
-
-  /**
-   * Appending buttons to a container
-   * @param {*} container - HTMLElement to append buttons to
-   */
-  appendAllButtonsToContainer(container) {
-    if (!(container instanceof HTMLElement)) {
-      throw new Error(`SideBar.appendAllButtonsToContainer requires HTMLElement, but was given: ${typeof(container)}!`);
-    }
-
-    this.buttons.forEach((button) => {
-      container.appendChild(button);
-    });
   }
 
   setButtonClickHandler(someFunction) {
@@ -111,7 +96,6 @@ function activateSidebarButtons() {
     const button = document.createElement('button');
     button.textContent = fortune;
 
-    button.style.top = `${index*buttonHeight}px`;
     button.addEventListener('click', () => {
       openFortuneInput(index);
       openSound.play();
