e8743ddeaa1edb25df98ba42059081d4b206f2e7
adarsh249 <adarshpatel249@gmail.com>
2023-06-07 09:22:42 -0700

    Added 4 more unit tests

--- assets/__tests__/origami.test.js
+++ assets/__tests__/origami.test.js
@@ -1,5 +1,9 @@
 import {getDarkerShade, getOriginalColor} from '../scripts/utilities/colors.js';
 describe('Testing different flaps on getDarkerShade()', () => {
+  /*
+  So these specifc 4 tests pass, but I'm not sure why based
+  off the file in Origami.js
+  */
   test('blue flap darkens to #0D6E8E', () => {
     expect(getDarkerShade('#118AB2')).toBe('#0D6E8E');
   });
@@ -11,5 +15,21 @@ describe('Testing different flaps on getDarkerShade()', () => {
   });
   test('yellow flap darkens to #CCA751', () => {
     expect(getDarkerShade('#FFD166')).toBe('#CCA751');
+  });
+});
+describe('Testing different flaps on getOriginalColor()', () => {
+  test('#0D6E8E lightens back to blue', () => {
+    expect(getOriginalColor('#0D6E8E')).toBe('#118AB2');
+  });
+  // failing, idk why
+  test('#BF3858 lightens back to red', () => {
+    expect(getOriginalColor('#BF3858')).toBe('#962B44');
+  });
+  // failing idk why
+  test('#04AB80 lightens back to green', () => {
+    expect(getOriginalColor('#04AB80')).toBe('#00664C');
+  });
+  test('#CCA751 lightens back to yellow', () => {
+    expect(getOriginalColor('#CCA751')).toBe('#FFD166');
   });
 });
